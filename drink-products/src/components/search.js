b06c1b44838f8e295f09707c100f8346f2b08977
David Xian <david.xianfei@gmail.com>
2021-03-29 17:46:33 +1100

    Add Search Component and adjust layout

---
+++ drink-products/src/components/search.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Row, Col} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export default class Search extends Component{
+  render() {
+    return (
+      <Row className="search">
+        <Col className = "md-3 sm-12 offset-md-9">
+            <input type="text" placeholder="Search products"/>
+             <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+        </Col>
+      </Row>
+    )
+  }
+}
