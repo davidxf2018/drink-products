df12b51ec9465c59cd0de33c72dcf8f0b449d9d4
David Xian <david.xianfei@gmail.com>
2021-03-29 12:08:43 +1100

    Call Filter in main section and adjust layout

--- drink-products/src/App.js
+++ drink-products/src/App.js
@@ -3,23 +3,45 @@ import data from './shared/data.json';
 
 import './App.css';
 
-import {Container,Navbar} from 'reactstrap';
+import {Container} from 'reactstrap';
 import Products from './components/products';
+import Filter from './components/filter';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      products: data
+      products: data,
+      type: ""
+    }
+  }
+
+  filterProducts = (e) => {
+    const value = e.target.value;
+    console.log('v',value);
+    if(value === "" || value === "All"){
+      this.setState({
+        type: "",
+        products:data
+      })
+    }
+    else {
+      console.log('d',data);
+      this.setState({
+        type: value,
+        products: data.filter(
+          (product) => product.type === value
+        )
+      })
     }
   }
   render() {
     return (
       <div className="App">
         <Container>
-          <Navbar>
-          test
-          </Navbar>
+          <Filter count={this.state.products.length}
+           type={this.state.type}
+           filterProducts={this.filterProducts}></Filter>
         </Container>
         <Container>
           <Products products={this.state.products}></Products>
